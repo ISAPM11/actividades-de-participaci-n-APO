7371d69943774e0cf48af01bc0a63080e3e637cb
ISAPINO <isapinomena00@gmail.com>
2024-08-05 00:38:10 -0500

    c6

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,1 +1,7 @@
 #Crear un programa que calcule la suma de los números en una lista dada.
+def sumar_lista(numeros):
+    return sum(numeros)
+
+lista_de_numeros = [80, 2, 47, 74, 225]
+suma = sumar_lista(lista_de_numeros)
+print(f"La suma de los números en la lista definida {lista_de_numeros} es {suma}.")
