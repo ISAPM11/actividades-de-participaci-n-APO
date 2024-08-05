ec531c53d971cd40868f91b21e082b1a39152123
ISAPINO <isapinomena00@gmail.com>
2024-08-05 00:59:37 -0500

    c8

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -1,1 +1,9 @@
 #Crear una función que invierta el orden de los elementos en una lista dada.
+def invertir_lista_dada(lista):
+    return lista[::-1]
+
+lista_numeros = [45, 40, 39, 18, 13]
+lista_invertida = invertir_lista_dada(lista_numeros)
+
+print(f"La lista original es: {lista_numeros}")
+print(f"La lista invertida es: {lista_invertida}")
