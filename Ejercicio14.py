e8f31bf917d56450f4801a3a5f651779b428ac01
ISAPINO <isapinomena00@gmail.com>
2024-08-05 01:16:59 -0500

    c14

--- Ejercicio14.py
+++ Ejercicio14.py
@@ -1,1 +1,16 @@
 #Escribir una función que calcule la media aritmética de una lista de números.
+def media_aritmetica(lista):
+
+    if not lista:
+        raise ValueError("La lista no puede estar vacía.")
+    
+    suma = sum(lista)
+    cantidad = len(lista)
+    return suma / cantidad
+
+lista_numeros = [110, 30, 19, 4, 18]
+try:
+    resultado = media_aritmetica(lista_numeros)
+    print(f"La media aritmética de la lista {lista_numeros} es {resultado:.2f}.")
+except ValueError as e:
+    print(e)
