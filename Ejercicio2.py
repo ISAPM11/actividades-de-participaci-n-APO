b88e152c27e089439ad700548b835995d739e7f7
ISAPINO <isapinomena00@gmail.com>
2024-08-04 23:31:58 -0500

    c2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,6 +2,6 @@
 import math
 def area_circulo(radio):
      return math.pi * (radio ** 2)
-radio = float(input("Ingrese el radio del círculo: "))
+radio = float(input("Ingresa el radio del círculo: "))
 area = area_circulo(radio)
-print(f"El área del círculo con radio {radio} es {area:.2f}")
+print(f"El área del círculo con radio {radio} es de {area:.2f}")
