da4983a6edbd6e797ea08e4308e62b833eef0f97
ISAPINO <isapinomena00@gmail.com>
2024-08-05 01:19:30 -0500

    c15

--- Ejercicio15.py
+++ Ejercicio15.py
@@ -1,1 +1,12 @@
 #Crear un programa que pida al usuario ingresar una cadena de texto y determine si es un palíndromo o no.
+def es_palindromo(cadena):
+    cadena = cadena.lower().replace(" ", "")
+    
+    return cadena == cadena[::-1]
+
+texto = input("Ingrese una cadena de texto: ")
+
+if es_palindromo(texto):
+    print(f"La cadena '{texto}' es un palíndromo.")
+else:
+    print(f"La cadena '{texto}' no es un palíndromo.")
