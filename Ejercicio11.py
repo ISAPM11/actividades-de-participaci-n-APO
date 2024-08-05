a368ded574b3124179e98ed482147cac1b7a7673
ISAPINO <isapinomena00@gmail.com>
2024-08-05 01:10:10 -0500

    c11

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,1 +1,7 @@
 #Crear un programa que genere una lista de números pares entre 1 y 100.
+def generar_pares(inicio, fin):
+    
+    return [num for num in range(inicio, fin + 1) if num % 2 == 0]
+
+numeros_pares = generar_pares(1, 100)
+print(f"Los números pares entre 1 y 100 son: {numeros_pares}")
