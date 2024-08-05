7948a256433f8249c1dbe63edd7257cee525cba2
ISAPINO <isapinomena00@gmail.com>
2024-08-05 00:33:47 -0500

    c5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,1 +1,8 @@
 #Crear una función que convierta grados Fahrenheit a grados Celsius.
+def fahrenheit_a_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5.0/9.0
+    return celsius
+
+fahrenheit = float(input("Ingrese la temperatura en grados Fahrenheit: "))
+celsius = fahrenheit_a_celsius(fahrenheit)
+print(f"{fahrenheit} grados Fahrenheit son {celsius:.2f} grados Celsius.")
